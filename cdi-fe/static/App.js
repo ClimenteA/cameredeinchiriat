bbc6c5aae23a5f5c058f4a810b10fc71aeecdcb3
alincmt <aclimente@ymail.com>
2020-05-20 22:41:50 +0300

    Add files via upload

--- cdi-fe/static/App.js
+++ cdi-fe/static/App.js
@@ -1,7 +1,10 @@
 import {type_motive} from "./src/Landing.js"
 import {toggle_menu} from "./src/Menu.js"
 
+import {toggle_search} from "./src/ToggleSearch.js"
+
 
 type_motive()
 toggle_menu()
+toggle_search()
 
